a44e69c794b850bd17b7503d820984f7a93aa5b9
abGhost.cyber <xplendo@gmail.com>
2021-04-12 14:51:23 +0800

    first commit

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
@@ -9,5 +9,44 @@ import kotlinx.coroutines.*
 
 @KtorExperimentalAPI
 fun main() {
+    val client = HttpClient {
+        install(WebSockets)
+    }
+    suspend fun DefaultWebSocketSession.outputMessages() {
+        try {
+            for (message in incoming) {
+                message as? Frame.Text ?: continue
+                println(message.readText())
+            }
+        } catch (e: Exception) {
+            println("Error occurred while receiving: ${e.localizedMessage}")
+        }
+    }
+
+    suspend fun DefaultWebSocketSession.inputMessages() {
+        while (true) {
+            val message = readLine() ?: ""
+            if (message.equals("exit", true)) return
+            try {
+                send(message)
+            } catch (e: Exception) {
+                println("Error occurred while receiving: ${e.localizedMessage}")
+            }
+        }
+    }
+    runBlocking {
+        client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
+            val messageOutputRoutine = launch { outputMessages() }
+            val userInputRoutine = launch { inputMessages() }
+            //wait for completion either "exit" or error
+            userInputRoutine.join()
+            messageOutputRoutine.cancelAndJoin()
+        }
+    }
+    client.close()
+    println("Connection closed. Goodbye!")
+
+
+
 
 }
--- server/src/main/kotlin/com/jetbrains/handson/chat/server/Application.kt
+++ server/src/main/kotlin/com/jetbrains/handson/chat/server/Application.kt
@@ -5,11 +5,51 @@ import io.ktor.http.cio.websocket.*
 import io.ktor.routing.*
 import io.ktor.websocket.*
 import java.util.*
+import kotlin.collections.LinkedHashSet
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused")
 fun Application.module() {
-
+    install(WebSockets)
+    routing {
+//        webSocket("/chat") {
+//            send("You are connected")
+//            for (frame in incoming) {
+//                frame as? Frame.Text ?: continue
+//                val receivedText = frame.readText()
+//                send("You said $receivedText")
+//            }
+//        }
+        //create a thread safe collections of connections
+        val connections = Collections.synchronizedSet<Connection>(LinkedHashSet())
+        webSocket("/chat") {
+            println("Adding user!")
+            //create a users connection object
+            val thisConnection = Connection(this)
+            //add connection to the thread safe collections
+            connections.add(thisConnection)
+            if(connections.isNotEmpty()){
+                connections.filter { it.name != thisConnection.name }
+                        .forEach { it.session.send("${thisConnection.name} joined the chat") }
+            }
+            try {
+                send("You are connected! There are ${connections.count()} users here, say hello to others 😁")
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    val textWithUserName = "[${thisConnection.name}]: $receivedText"
+                    connections.filter { it.name != thisConnection.name }
+                            .forEach { it.session.send(textWithUserName) }
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                //remove user on connection terminated
+                println("Removing ${thisConnection.name}!")
+                connections.remove(thisConnection)
+            }
+        }
+    }
 }
